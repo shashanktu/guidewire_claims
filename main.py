4c09c451553d02cf7844bacf85c434db13281971
shashanktu <shashank.tudum@valuemomentum.com>
2026-02-13 11:38:37 +0530

    named api

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import json
 
-app = FastAPI()
+app = FastAPI(title="Guidewire Claims API")
 
 app.add_middleware(
     CORSMiddleware,
